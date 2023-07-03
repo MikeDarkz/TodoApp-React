3ffa19b8655847a44ffaec11b9a1b0a243aab6a0
MikeDarkz <63819998+MikeDarkz@users.noreply.github.com>
2023-07-03 08:25:06 -0500

    Se agrega formulario de actualización de tarea con carga de datos

--- src/ActualizarTodo.js
+++ src/ActualizarTodo.js
@@ -1,27 +1,51 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom/dist';
 function ActualizarTodo() {
-    return (
-        <div class="form-container">
-        <form>
-          <div class="form-group">
-            <label for="id">ID:</label>
-            <input type="text" id="id" name="id" required/>
-          </div>
-          <div class="form-group">
-            <label for="title">Título:</label>
-            <input type="text" id="title" name="title" required/>
-          </div>
-          <div class="form-group">
-            <label for="completed">Completado:</label>
-            <input type="checkbox" id="completed" name="completed"/>
-          </div>
-          <div class="form-group">
-            <input type="submit" value="Enviar"/>
-          </div>
-        </form>
-      </div>
-    );
-  }
+
+  const { item } = useParams();
+  const [ tarea, setTarea ] = useState([]);
+  const tareaActualizar = JSON.parse(decodeURIComponent(item));
   
-  export default ActualizarTodo;
+  useEffect(() => {
+    setTarea(tareaActualizar);
+  }, []);
+
+  const handleId = (event) => {
+    tarea.id = event.target.value
+  };
+
+  const handleTitle = (event) => {
+    tarea.tile = event.target.value
+  };
+
+  const handleCompleted = (event) => {
+    tarea.completed = event.target.value
+  };
+
+  return (
+      <div className="form-container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="id">ID:</label>
+          <input type="text" id="id" name="id" value={tarea.id} onChange={() => handleId} required/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="title">Título:</label>
+          <input type="text" id="title" name="title" value={tarea.title} onChange={() => handleTitle} required/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="completed">Completado:</label>
+          <input type="checkbox" id="completed" name="completed" onChange={() => handleCompleted} checked={tarea.completed === 1}/>
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Enviar"/>
+        </div>
+      </form>
+    </div>
+  );
+}
+  
+export default ActualizarTodo;
   
