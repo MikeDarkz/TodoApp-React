855576c5863923993a7bd8462f40c65efef9adab
MikeDarkz <gualterosmichael@hotmail.com>
2023-07-07 19:00:19 -0500

    Ajuste en index

--- src/index.js
+++ src/index.js
@@ -28,7 +28,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
